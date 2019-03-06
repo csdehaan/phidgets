ac17c8da4ffe9c7f0f888efa9fa51eade4ed88a5
Craig DeHaan <craig.s.dehaan@gmail.com>
2019-03-06 18:52:15 -0500

    Convert GPS to ver 2.2

--- lib/phidgets/gps.rb
+++ lib/phidgets/gps.rb
@@ -3,34 +3,42 @@
 module Phidgets
   class GPS < Common
 
-    unless RUBY_VERSION < '1.9.0'
-
-      # call-seq:
-      #   setOnPositionChangeHandler(proc=nil, &block)
-      #
-      # Sets a position change event handler. This is called when any of latitude, longitude, or altitude change.
-      #
-      def setOnPositionChangeHandler(cb_proc = nil, &cb_block)
-        @on_position_change_thread.kill if defined? @on_position_change_thread
-        callback = cb_proc || cb_block
-        @on_position_change_thread = Thread.new {ext_setOnPositionChangeHandler(callback)}
-      end
-
-      # call-seq:
-      #   setOnPositionFixStatusChangeHandler(proc=nil, &block)
-      #
-      # Sets a position fix status change event handler. This is called when a position fix is aquired or lost.
-      #
-      def setOnPositionFixStatusChangeHandler(cb_proc = nil, &cb_block)
-        @on_position_fix_status_change_thread.kill if defined? @on_position_fix_status_change_thread
-        callback = cb_proc || cb_block
-        @on_position_fix_status_change_thread = Thread.new {ext_setOnPositionFixStatusChangeHandler(callback)}
-      end
-
-      alias :on_position_change :setOnPositionChangeHandler
-      alias :on_position_fix_status_change :setOnPositionFixStatusChangeHandler
-
+    # call-seq:
+    #   setOnHeadingChangeHandler(proc=nil, &block)
+    #
+    # Assigns a handler that will be called when the HeadingChange event occurs.
+    #
+    def setOnHeadingChangeHandler(cb_proc = nil, &cb_block)
+      @on_heading_change_thread.kill if defined? @on_heading_change_thread and @on_heading_change_thread.alive?
+      callback = cb_proc || cb_block
+      @on_heading_change_thread = Thread.new {ext_setOnHeadingChangeHandler(callback)}
     end
+
+    # call-seq:
+    #   setOnPositionChangeHandler(proc=nil, &block)
+    #
+    # Assigns a handler that will be called when the PositionChange event occurs.
+    #
+    def setOnPositionChangeHandler(cb_proc = nil, &cb_block)
+      @on_position_change_thread.kill if defined? @on_position_change_thread and @on_position_change_thread.alive?
+      callback = cb_proc || cb_block
+      @on_position_change_thread = Thread.new {ext_setOnPositionChangeHandler(callback)}
+    end
+
+    # call-seq:
+    #   setOnPositionFixStateChangeHandler(proc=nil, &block)
+    #
+    # Assigns a handler that will be called when the PositionFixStateChange event occurs.
+    #
+    def setOnPositionFixStateChangeHandler(cb_proc = nil, &cb_block)
+      @on_position_fix_state_change_thread.kill if defined? @on_position_fix_state_change_thread and @on_position_fix_state_change_thread.alive?
+      callback = cb_proc || cb_block
+      @on_position_fix_state_change_thread = Thread.new {ext_setOnPositionFixStateChangeHandler(callback)}
+    end
+
+    alias :on_heading_change :setOnHeadingChangeHandler
+    alias :on_position_change :setOnPositionChangeHandler
+    alias :on_position_fix_state_change :setOnPositionFixStateChangeHandler
 
   end
 end
