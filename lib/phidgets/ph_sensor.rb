f99c5d3d3ae67adfbc23dabee93bf9225a4c03a0
Craig DeHaan <craig.s.dehaan@gmail.com>
2019-03-06 18:53:21 -0500

    Convert PHSensor to ver 2.2

--- lib/phidgets/ph_sensor.rb
+++ lib/phidgets/ph_sensor.rb
@@ -3,22 +3,18 @@
 module Phidgets
   class PHSensor < Common
 
-    unless RUBY_VERSION < '1.9.0'
-
       # call-seq:
       #   setOnPHChangeHandler(proc=nil, &block)
       #
-      # Set a PH change handler. This is called when the PH changes by more then the change trigger.
+      # Assigns a handler that will be called when the PHChange event occurs.
       #
       def setOnPHChangeHandler(cb_proc = nil, &cb_block)
-        @on_ph_change_thread.kill if defined? @on_ph_change_thread
+        @on_ph_change_thread.kill if defined? @on_ph_change_thread and @on_ph_change_thread.alive?
         callback = cb_proc || cb_block
         @on_ph_change_thread = Thread.new {ext_setOnPHChangeHandler(callback)}
       end
 
       alias :on_ph_change :setOnPHChangeHandler
-
-    end
 
   end
 end
