21a822c176b8b3630f8902b54ff97a48cee9cee7
Craig DeHaan <craig.s.dehaan@gmail.com>
2019-03-14 20:00:30 -0400

    Add check to see if on_state_change thread is alive

--- lib/phidgets/digital_input.rb
+++ lib/phidgets/digital_input.rb
@@ -9,7 +9,7 @@ module Phidgets
     # Set a digital input change handler. This is called when a digital input changes.
     #
     def setOnStateChangeHandler(cb_proc = nil, &cb_block)
-      @on_state_change_thread.kill if defined? @on_state_change_thread
+      @on_state_change_thread.kill if defined? @on_state_change_thread and @on_state_change_thread.alive?
       callback = cb_proc || cb_block
       @on_state_change_thread = Thread.new {ext_setOnStateChangeHandler(callback)}
     end
