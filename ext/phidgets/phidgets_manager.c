a42bde86d52c3654a566c3c3f859198d6f9bb71f
Craig DeHaan <craig.s.dehaan@gmail.com>
2019-03-31 16:02:05 -0400

    Add Manager

--- ext/phidgets/phidgets_manager.c
+++ ext/phidgets/phidgets_manager.c
@@ -1,28 +1,124 @@
 
 #include "phidgets.h"
 
-#if 0
-VALUE ph_manager_allocate(VALUE klass);
-void  ph_manager_free(ph_data_t *ph);
-VALUE ph_manager_init(VALUE self);
-VALUE ph_manager_open(VALUE self);
-VALUE ph_manager_open_remote(VALUE self, VALUE server_id, VALUE password);
-VALUE ph_manager_open_remote_ip(VALUE self, VALUE address, VALUE port, VALUE password);
-VALUE ph_manager_close(VALUE self);
-VALUE ph_manager_get_server_id(VALUE self);
-VALUE ph_manager_get_server_address(VALUE self);
-VALUE ph_manager_get_server_status(VALUE self);
-VALUE ph_manager_get_attached_devices(VALUE self);
-VALUE ph_manager_instance_from_class(Phidget_DeviceClass dev_class);
 
-VALUE ph_manager_set_on_attach_handler(VALUE self, VALUE handler);
-VALUE ph_manager_set_on_detach_handler(VALUE self, VALUE handler);
-VALUE ph_manager_set_on_server_connect_handler(VALUE self, VALUE handler);
-VALUE ph_manager_set_on_server_disconnect_handler(VALUE self, VALUE handler);
-int ph_manager_on_attach(PhidgetHandle phid, void *userPtr);
-int ph_manager_on_detach(PhidgetHandle phid, void *userPtr);
-int ph_manager_on_server_connect(PhidgetManagerHandle phid, void *userPtr);
-int ph_manager_on_server_disconnect(PhidgetManagerHandle phid, void *userPtr);
+void ph_manager_free(ph_data_t *ph) {
+  if (ph && ph->handle) {
+    PhidgetManager_close((PhidgetManagerHandle)ph->handle);
+    PhidgetManager_delete((PhidgetManagerHandle *)(&(ph->handle)));
+    ph->handle = NULL;
+  }
+  if(ph) {
+    sem_destroy(&ph->attach_callback.handler_ready);
+    sem_destroy(&ph->attach_callback.callback_called);
+    sem_destroy(&ph->detach_callback.handler_ready);
+    sem_destroy(&ph->detach_callback.callback_called);
+    xfree(ph);
+  }
+}
+
+
+VALUE ph_manager_allocate(VALUE klass) {
+  ph_data_t *ph;
+  VALUE self = Data_Make_Struct(klass, ph_data_t, 0, ph_manager_free, ph);
+  memset(ph, 0, sizeof(ph_data_t));
+  sem_init(&ph->attach_callback.handler_ready, 0, 0);
+  sem_init(&ph->attach_callback.callback_called, 0, 0);
+  sem_init(&ph->detach_callback.handler_ready, 0, 0);
+  sem_init(&ph->detach_callback.callback_called, 0, 0);
+  return self;
+}
+
+VALUE ph_manager_init(VALUE self) {
+  ph_data_t *ph = get_ph_data(self);
+  ph_raise(PhidgetManager_create((PhidgetManagerHandle *)(&(ph->handle))));
+  return self;
+}
+
+VALUE ph_manager_open(VALUE self) {
+  ph_raise(PhidgetManager_open((PhidgetManagerHandle)get_ph_handle(self)));
+  return Qnil;
+}
+
+VALUE ph_manager_close(VALUE self) {
+  ph_raise(PhidgetManager_close((PhidgetManagerHandle)get_ph_handle(self)));
+  return Qnil;
+}
+
+
+void CCONV ph_manager_on_attach(PhidgetManagerHandle phid, void *userPtr, PhidgetHandle channel) {
+  ph_callback_data_t *callback_data = ((ph_callback_data_t *)userPtr);
+  int32_t device_serial_number;
+  Phidget_ChannelClass channel_class;
+  int channel_index;
+  const char *channel_name;
+  Phidget_getDeviceSerialNumber(channel, &device_serial_number);
+  Phidget_getChannelClass(channel, &channel_class);
+  Phidget_getChannel(channel, &channel_index);
+  Phidget_getChannelName(channel, &channel_name);
+  while(sem_wait(&callback_data->handler_ready)!=0) {};
+  callback_data->arg1 = INT2NUM(device_serial_number);
+  callback_data->arg2 = INT2NUM(channel_class);
+  callback_data->arg3 = INT2NUM(channel_index);
+  callback_data->arg4 = rb_str_new2(channel_name);
+  sem_post(&callback_data->callback_called);
+}
+
+
+VALUE ph_manager_set_on_attach_handler(VALUE self, VALUE handler) {
+  ph_data_t *ph = get_ph_data(self);
+  ph_callback_data_t *callback_data = &ph->attach_callback;
+  if( TYPE(handler) == T_NIL ) {
+    callback_data->callback = T_NIL;
+    callback_data->exit = true;
+    ph_raise(PhidgetManager_setOnAttachHandler((PhidgetManagerHandle)ph->handle, NULL, (void *)NULL));
+    sem_post(&callback_data->callback_called);
+  } else {
+    callback_data->exit = false;
+    callback_data->phidget = self;
+    callback_data->callback = handler;
+    ph_raise(PhidgetManager_setOnAttachHandler((PhidgetManagerHandle)ph->handle, ph_manager_on_attach, (void *)callback_data));
+    ph_callback_thread(callback_data);
+  }
+  return Qnil;
+}
+
+
+void CCONV ph_manager_on_detach(PhidgetManagerHandle phid, void *userPtr, PhidgetHandle channel) {
+  ph_callback_data_t *callback_data = ((ph_callback_data_t *)userPtr);
+  int32_t device_serial_number;
+  Phidget_ChannelClass channel_class;
+  int channel_index;
+  Phidget_getDeviceSerialNumber(channel, &device_serial_number);
+  Phidget_getChannelClass(channel, &channel_class);
+  Phidget_getChannel(channel, &channel_index);
+  while(sem_wait(&callback_data->handler_ready)!=0) {};
+  callback_data->arg1 = INT2NUM(device_serial_number);
+  callback_data->arg2 = INT2NUM(channel_class);
+  callback_data->arg3 = INT2NUM(channel_index);
+  callback_data->arg4 = Qnil;
+  sem_post(&callback_data->callback_called);
+}
+
+
+VALUE ph_manager_set_on_detach_handler(VALUE self, VALUE handler) {
+  ph_data_t *ph = get_ph_data(self);
+  ph_callback_data_t *callback_data = &ph->detach_callback;
+  if( TYPE(handler) == T_NIL ) {
+    callback_data->callback = T_NIL;
+    callback_data->exit = true;
+    ph_raise(PhidgetManager_setOnDetachHandler((PhidgetManagerHandle)ph->handle, NULL, (void *)NULL));
+    sem_post(&callback_data->callback_called);
+  } else {
+    callback_data->exit = false;
+    callback_data->phidget = self;
+    callback_data->callback = handler;
+    ph_raise(PhidgetManager_setOnDetachHandler((PhidgetManagerHandle)ph->handle, ph_manager_on_detach, (void *)callback_data));
+    ph_callback_thread(callback_data);
+  }
+  return Qnil;
+}
+
 
 
 void Init_manager() {
@@ -36,324 +132,23 @@ void Init_manager() {
    * Creates a Phidget Manager object.
    */
   rb_define_method(ph_manager, "initialize", ph_manager_init, 0);
-  rb_define_private_method(ph_manager, "ext_open", ph_manager_open, 0);
 
-  /* Document-method: openRemote
-   * call-seq: openRemote(server_id, password)
+  /* Document-method: open
+   * call-seq: open
    *
-   * Opens a Phidget manager remotely by ServerID. Note that this requires Bonjour
-   * (mDNS) to be running on both the host and the server.
+   * Opens the Phidget Manager.
+   * Be sure to register Attach and Detach event handlers for the Manager before opening it, to ensure you program doesn't miss the events reported for devices already connected to your system.
    */
-  rb_define_method(ph_manager, "openRemote", ph_manager_open_remote, 2);
-
-  /* Document-method: openRemoteIP
-   * call-seq: openRemoteIP(address, port, password)
-   *
-   * Opens a Phidget manager remotely by address and port.
-   */
-  rb_define_method(ph_manager, "openRemoteIP", ph_manager_open_remote_ip, 3);
+  rb_define_method(ph_manager, "open", ph_manager_open, 0);
 
   /* Document-method: close
    * call-seq: close
    *
-   * Closes a Phidget Manager.
+   * Closes a Phidget Manager that has been opened. PhidgetManage#close will release the Phidget Manager, and should be called prior to delete.
    */
   rb_define_method(ph_manager, "close", ph_manager_close, 0);
 
-  /* Document-method: getServerID
-   * call-seq: getServerID -> server_id
-   *
-   * Gets the server ID of a remotely opened Phidget Manager. This will fail if the manager was opened locally.
-   */
-  rb_define_method(ph_manager, "getServerID", ph_manager_get_server_id, 0);
-
-  /* Document-method: getServerAddress
-   * call-seq: getServerAddress -> [address, port]
-   *
-   * Gets the address and port of a remotely opened Phidget Manager. This will fail if the manager was opened locally.
-   */
-  rb_define_method(ph_manager, "getServerAddress", ph_manager_get_server_address, 0);
-
-  /* Document-method: getServerStatus
-   * call-seq: getServerStatus -> status
-   *
-   * Gets the connected to server status of a remotely opened Phidget Manager. This will fail if the manager was opened locally.
-   */
-  rb_define_method(ph_manager, "getServerStatus", ph_manager_get_server_status, 0);
-
-  /* Document-method: getAttachedDevices
-   * call-seq: getAttachedDevices -> [devices]
-   *
-   * Gets an array of all currently attached Phidgets.
-   */
-  rb_define_method(ph_manager, "getAttachedDevices", ph_manager_get_attached_devices, 0);
-
   rb_define_private_method(ph_manager, "ext_setOnAttachHandler", ph_manager_set_on_attach_handler, 1);
   rb_define_private_method(ph_manager, "ext_setOnDetachHandler", ph_manager_set_on_detach_handler, 1);
-  rb_define_private_method(ph_manager, "ext_setOnServerConnectHandler", ph_manager_set_on_server_connect_handler, 1);
-  rb_define_private_method(ph_manager, "ext_setOnServerDisconnectHandler", ph_manager_set_on_server_disconnect_handler, 1);
-
-  rb_define_alias(ph_manager, "open_remote", "openRemote");
-  rb_define_alias(ph_manager, "open_remote_ip", "openRemoteIP");
-  rb_define_alias(ph_manager, "server_id", "getServerID");
-  rb_define_alias(ph_manager, "server_address", "getServerAddress");
-  rb_define_alias(ph_manager, "server_status", "getServerStatus");
-  rb_define_alias(ph_manager, "attached_devices", "getAttachedDevices");
 }
 
-
-
-VALUE ph_manager_allocate(VALUE klass) {
-  ph_data_t *ph;
-  VALUE self = Data_Make_Struct(klass, ph_data_t, 0, ph_manager_free, ph);
-  memset(ph, 0, sizeof(ph_data_t));
-  return self;
-}
-
-void ph_manager_free(ph_data_t *ph) {
-  if (ph && ph->handle) {
-    PhidgetManager_close((PhidgetManagerHandle)ph->handle);
-    PhidgetManager_delete((PhidgetManagerHandle)ph->handle);
-    ph->handle = NULL;
-  }
-  if(ph) xfree(ph);
-}
-
-VALUE ph_manager_init(VALUE self) {
-  ph_data_t *ph = get_ph_data(self);
-  ph_raise(PhidgetManager_create((PhidgetManagerHandle *)(&(ph->handle))));
-  return self;
-}
-
-VALUE ph_manager_open(VALUE self) {
-  PhidgetManagerHandle handle = (PhidgetManagerHandle)get_ph_handle(self);
-  ph_raise(PhidgetManager_open(handle));
-  return Qnil;
-}
-
-VALUE ph_manager_open_remote(VALUE self, VALUE server_id, VALUE password) {
-  PhidgetManagerHandle handle = (PhidgetManagerHandle)get_ph_handle(self);
-  ph_raise(PhidgetManager_openRemote(handle, StringValueCStr(server_id), StringValueCStr(password)));
-  return Qnil;
-}
-
-VALUE ph_manager_open_remote_ip(VALUE self, VALUE address, VALUE port, VALUE password) {
-  PhidgetManagerHandle handle = (PhidgetManagerHandle)get_ph_handle(self);
-  ph_raise(PhidgetManager_openRemoteIP(handle, StringValueCStr(address), FIX2INT(port), StringValueCStr(password)));
-  return Qnil;
-}
-
-VALUE ph_manager_close(VALUE self) {
-  PhidgetManagerHandle handle = (PhidgetManagerHandle)get_ph_handle(self);
-  ph_raise(PhidgetManager_close(handle));
-  return Qnil;
-}
-
-VALUE ph_manager_get_server_id(VALUE self) {
-  PhidgetManagerHandle handle = (PhidgetManagerHandle)get_ph_handle(self);
-  const char *server_id;
-  ph_raise(PhidgetManager_getServerID(handle, &server_id));
-  return rb_str_new2(server_id);
-}
-
-VALUE ph_manager_get_server_address(VALUE self) {
-  PhidgetManagerHandle handle = (PhidgetManagerHandle)get_ph_handle(self);
-  const char *address;
-  int port;
-  ph_raise(PhidgetManager_getServerAddress(handle, &address, &port));
-  return rb_ary_new3(2, rb_str_new2(address), INT2FIX(port));
-}
-
-VALUE ph_manager_get_server_status(VALUE self) {
-  PhidgetManagerHandle handle = (PhidgetManagerHandle)get_ph_handle(self);
-  int server_status;
-  ph_raise(PhidgetManager_getServerStatus(handle, &server_status));
-  return INT2FIX(server_status);
-}
-
-VALUE ph_manager_get_attached_devices(VALUE self) {
-  PhidgetManagerHandle handle = (PhidgetManagerHandle)get_ph_handle(self);
-  PhidgetHandle *devices;
-  int count, i;
-  VALUE rb_hash = rb_hash_new();
-  ph_raise(PhidgetManager_getAttachedDevices(handle, &devices, &count));
-  for(i=0; i<count; i++) {
-    int serial;
-    Phidget_DeviceClass dev_class;
-    ph_raise(Phidget_getSerialNumber(devices[i], &serial));
-    ph_raise(Phidget_getDeviceClass(devices[i], &dev_class));
-    rb_hash_aset(rb_hash, INT2FIX(serial), ph_manager_instance_from_class(dev_class));
-  }
-  ph_raise(PhidgetManager_freeAttachedDevicesArray(devices));
-  return rb_hash;
-}
-
-VALUE ph_manager_instance_from_class(Phidget_DeviceClass dev_class) {
-  VALUE ph_module = rb_const_get(rb_cModule, rb_intern("Phidgets"));
-  VALUE klass;
-  VALUE argv[0];
-
-  switch( dev_class ) {
-    case PHIDCLASS_ACCELEROMETER:
-      klass = rb_const_get(ph_module, rb_intern("Accelerometer"));
-      break;
-    case PHIDCLASS_ADVANCEDSERVO:
-      klass = rb_const_get(ph_module, rb_intern("AdvancedServo"));
-      break;
-    case PHIDCLASS_ANALOG:
-      klass = rb_const_get(ph_module, rb_intern("Analog"));
-      break;
-    case PHIDCLASS_BRIDGE:
-      klass = rb_const_get(ph_module, rb_intern("Bridge"));
-      break;
-    case PHIDCLASS_ENCODER:
-      klass = rb_const_get(ph_module, rb_intern("Encoder"));
-      break;
-    case PHIDCLASS_FREQUENCYCOUNTER:
-      klass = rb_const_get(ph_module, rb_intern("FrequencyCounter"));
-      break;
-    case PHIDCLASS_GPS:
-      klass = rb_const_get(ph_module, rb_intern("GPS"));
-      break;
-    case PHIDCLASS_INTERFACEKIT:
-      klass = rb_const_get(ph_module, rb_intern("InterfaceKit"));
-      break;
-    case PHIDCLASS_IR:
-      klass = rb_const_get(ph_module, rb_intern("IR"));
-      break;
-    case PHIDCLASS_LED:
-      klass = rb_const_get(ph_module, rb_intern("LED"));
-      break;
-    case PHIDCLASS_MOTORCONTROL:
-      klass = rb_const_get(ph_module, rb_intern("MotorControl"));
-      break;
-    case PHIDCLASS_PHSENSOR:
-      klass = rb_const_get(ph_module, rb_intern("PhSensor"));
-      break;
-    case PHIDCLASS_RFID:
-      klass = rb_const_get(ph_module, rb_intern("RFID"));
-      break;
-    case PHIDCLASS_SERVO:
-      klass = rb_const_get(ph_module, rb_intern("Servo"));
-      break;
-    case PHIDCLASS_SPATIAL:
-      klass = rb_const_get(ph_module, rb_intern("Spatial"));
-      break;
-    case PHIDCLASS_STEPPER:
-      klass = rb_const_get(ph_module, rb_intern("Stepper"));
-      break;
-    case PHIDCLASS_TEMPERATURESENSOR:
-      klass = rb_const_get(ph_module, rb_intern("TemperatureSensor"));
-      break;
-    case PHIDCLASS_TEXTLCD:
-      klass = rb_const_get(ph_module, rb_intern("TextLCD"));
-      break;
-    case PHIDCLASS_TEXTLED:
-      klass = rb_const_get(ph_module, rb_intern("TextLED"));
-      break;
-    case PHIDCLASS_WEIGHTSENSOR:
-      klass = rb_const_get(ph_module, rb_intern("WeightSensor"));
-      break;
-    default:
-      klass = Qnil;
-  }
-
-  return rb_class_new_instance(0, argv, klass);
-}
-
-VALUE ph_manager_set_on_attach_handler(VALUE self, VALUE handler) {
-  ph_data_t *ph = get_ph_data(self);
-  ph_callback_data_t *callback_data = &ph->attach_callback;
-  if( TYPE(handler) == T_NIL ) {
-    callback_data->exit = true;
-    ph_raise(PhidgetManager_set_OnAttach_Handler((PhidgetManagerHandle)ph->handle, NULL, (void *)NULL));
-  } else {
-    callback_data->called = false;
-    callback_data->exit = false;
-    callback_data->phidget = self;
-    callback_data->callback = handler;
-    ph_raise(PhidgetManager_set_OnAttach_Handler((PhidgetManagerHandle)ph->handle, ph_manager_on_attach, (void *)callback_data));
-    ph_callback_thread(callback_data);
-  }
-  return Qnil;
-}
-
-VALUE ph_manager_set_on_detach_handler(VALUE self, VALUE handler) {
-  ph_data_t *ph = get_ph_data(self);
-  ph_callback_data_t *callback_data = &ph->detach_callback;
-  if( TYPE(handler) == T_NIL ) {
-    callback_data->exit = true;
-    ph_raise(PhidgetManager_set_OnDetach_Handler((PhidgetManagerHandle)ph->handle, NULL, (void *)NULL));
-  } else {
-    callback_data->called = false;
-    callback_data->exit = false;
-    callback_data->phidget = self;
-    callback_data->callback = handler;
-    ph_raise(PhidgetManager_set_OnDetach_Handler((PhidgetManagerHandle)ph->handle, ph_manager_on_detach, (void *)callback_data));
-    ph_callback_thread(callback_data);
-  }
-  return Qnil;
-}
-
-VALUE ph_manager_set_on_server_connect_handler(VALUE self, VALUE handler) {
-  ph_data_t *ph = get_ph_data(self);
-  ph_callback_data_t *callback_data = &ph->server_connect_callback;
-  if( TYPE(handler) == T_NIL ) {
-    callback_data->exit = true;
-    ph_raise(PhidgetManager_set_OnServerConnect_Handler((PhidgetManagerHandle)ph->handle, NULL, (void *)NULL));
-  } else {
-    callback_data->called = false;
-    callback_data->exit = false;
-    callback_data->phidget = self;
-    callback_data->callback = handler;
-    ph_raise(PhidgetManager_set_OnServerConnect_Handler((PhidgetManagerHandle)ph->handle, ph_manager_on_server_connect, (void *)callback_data));
-    ph_callback_thread(callback_data);
-  }
-  return Qnil;
-}
-
-VALUE ph_manager_set_on_server_disconnect_handler(VALUE self, VALUE handler) {
-  ph_data_t *ph = get_ph_data(self);
-  ph_callback_data_t *callback_data = &ph->server_disconnect_callback;
-  if( TYPE(handler) == T_NIL ) {
-    callback_data->exit = true;
-    ph_raise(PhidgetManager_set_OnServerDisconnect_Handler((PhidgetManagerHandle)ph->handle, NULL, (void *)NULL));
-  } else {
-    callback_data->called = false;
-    callback_data->exit = false;
-    callback_data->phidget = self;
-    callback_data->callback = handler;
-    ph_raise(PhidgetManager_set_OnServerDisconnect_Handler((PhidgetManagerHandle)ph->handle, ph_manager_on_server_disconnect, (void *)callback_data));
-    ph_callback_thread(callback_data);
-  }
-  return Qnil;
-}
-
-
-int ph_manager_on_attach(PhidgetHandle phid, void *userPtr) {
-  ph_callback_data_t *callback_data = ((ph_callback_data_t *)userPtr);
-  callback_data->called = true;
-  return EPHIDGET_OK;
-}
-
-
-int ph_manager_on_detach(PhidgetHandle phid, void *userPtr) {
-  ph_callback_data_t *callback_data = ((ph_callback_data_t *)userPtr);
-  callback_data->called = true;
-  return EPHIDGET_OK;
-}
-
-
-int ph_manager_on_server_connect(PhidgetManagerHandle phid, void *userPtr) {
-  ph_callback_data_t *callback_data = ((ph_callback_data_t *)userPtr);
-  callback_data->called = true;
-  return EPHIDGET_OK;
-}
-
-
-int ph_manager_on_server_disconnect(PhidgetManagerHandle phid, void *userPtr) {
-  ph_callback_data_t *callback_data = ((ph_callback_data_t *)userPtr);
-  callback_data->called = true;
-  return EPHIDGET_OK;
-}
-#endif
--- test/test_manager.rb
+++ test/test_manager.rb
@@ -2,92 +2,23 @@ require File.dirname(__FILE__) + '/test_helper.rb'
 
 class TestPhidgetsManager < Test::Unit::TestCase
 
+  def setup
+    @phidget = Phidgets::Manager.new
+  end
+
   def test_open
-    manager = Phidgets::Manager.new
-    assert_nothing_raised {manager.open}
-    manager.close
-  end
-
-  def test_open_with_hash_1
-    manager = Phidgets::Manager.new
-    assert_nothing_raised {manager.open :server_id => 'localhost', :password => ''}
-    manager.close
-  end
-
-  def test_open_with_hash_2
-    manager = Phidgets::Manager.new
-    assert_nothing_raised {manager.open :address => 'localhost', :port => 25000, :password => ''}
-    manager.close
-  end
-
-  def test_open_remote
-    manager = Phidgets::Manager.new
-    assert_nothing_raised {manager.open_remote 'localhost', 'password'}
-    manager.close
-  end
-
-  def test_open_remote_ip
-    manager = Phidgets::Manager.new
-    assert_nothing_raised {manager.open_remote_ip 'localhost', 25000, 'password'}
-    manager.close
-  end
-
-  def test_close
-    manager = Phidgets::Manager.new
-    manager.open
-    assert_nothing_raised {manager.close}
-  end
-
-  def test_get_server_id
-    manager = Phidgets::Manager.new
-    manager.open_remote 'localhost', 'password'
-    assert_equal('localhost', manager.server_id)
-    manager.close
-  end
-
-  def test_get_server_address
-    manager = Phidgets::Manager.new
-    manager.open_remote_ip 'localhost', 25000, 'password'
-    assert_equal(['localhost',25000], manager.server_address)
-    manager.close
-  end
-
-  def test_get_server_status
-    manager = Phidgets::Manager.new
-    manager.open_remote 'localhost', 'password'
-    assert_equal(Phidgets::NOTATTACHED, manager.server_status)
-    manager.close
-  end
-
-  def test_get_attached_devices
-    manager = Phidgets::Manager.new
     assert_nothing_raised do
-      devices = manager.attached_devices
-      assert_equal(0, devices.count)
+      @phidget.open
+      @phidget.close
     end
-    manager.close
   end
 
-  unless RUBY_VERSION < '1.9.0'
-    def test_set_on_attach
-      manager = Phidgets::Manager.new
-      assert_nothing_raised {manager.on_attach {puts 'hello'}}
-    end
+  def test_set_on_attach
+    assert_nothing_raised {@phidget.on_attach {puts 'phidget_attached'}}
+  end
 
-    def test_set_on_detach
-      manager = Phidgets::Manager.new
-      assert_nothing_raised {manager.on_detach {puts 'goodbye'}}
-    end
-
-    def test_set_on_server_connect
-      manager = Phidgets::Manager.new
-      assert_nothing_raised {manager.on_server_connect {puts 'hello'}}
-    end
-
-    def test_set_on_server_disconnect
-      manager = Phidgets::Manager.new
-      assert_nothing_raised {manager.on_server_disconnect {puts 'goodbye'}}
-    end
+  def test_set_on_detach
+    assert_nothing_raised {@phidget.on_detach {puts 'phidget_detached'}}
   end
 
 end
